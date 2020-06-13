e59744b55288e6a7f15aba7643c31c9db13dcc1a
Abubakr280492 <you@example.com>
2020-06-13 11:58:59 +0500

    Promisses

---
+++ Promisis and Asynchoronous/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+function display(data){console.log(data)}
+function printhello(){console.log("Hello")}
+function blockFor300ms(){/*BLOCK JS THREAD FOR 300 MS */}
+
+setTimeout(printHello,0);
+
+const future = fetch('https://twitter.com/will/tweets/1')
+futureData.then(display)
+
+blockFor300ms()
+console.log("Me frist")
