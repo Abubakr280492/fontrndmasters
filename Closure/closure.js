781ffe867b6ed03424ab78b46147caa901745f1f
Abubakr280492 <you@example.com>
2020-06-14 01:14:01 +0500

    closure

---
+++ Closure/closure.js
@@ -0,0 +1,28 @@
+
+
+
+
+// they forget privioust run value 
+const mutiplyBy2 = x => x*2
+
+
+const output = mutiplyBy2(7); //14
+const newOutput = mutiplyBy2(3); //6
+
+
+const functionCreator = () => {
+    let counter = 0 //we save counter in  local memory 
+    const add3 = (num) => {   // declaaare func 3 and return it    num : 2
+        const result = num+3  // 5
+        return result // 5 to result
+    }
+    return add3
+}
+
+const generatedFunc = functionCreator()  //new execution context  were saved in generatedFunc
+
+const result = generatedFunc(2) //5   result = generateFunc's name is add3
+
+const add3 = x => x+3
+const div5 = x => x/5
+
